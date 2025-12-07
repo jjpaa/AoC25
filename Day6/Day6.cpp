d65427ec32f5110330698bd70949ea74ef491f03
JJPaa <juhojarvenpaa98@gmail.com>
2025-12-07 13:32:09 +0200

    Day6 temp

--- Day6/Day6.cpp
+++ Day6/Day6.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream MyReadFile("input2.txt");
+    vector<vector<string>> data = {};
+
+    while (true)
+    {
+        string input;
+        getline(MyReadFile, input);
+
+        if (input == "")
+        {
+            break;
+        }
+
+        vector<string> row = {};
+        string newValueToAdd = "";
+
+        // Do for each input value.
+        for (int i = 0; i < input.length(); i++)
+        {
+            char inputChar = input[i];
+
+            if (inputChar == '*')
+            {
+
+            }
+            else if (inputChar == '+')
+            {
+
+            }
+            else if (inputChar == ' ')
+            {
+                if (newValueToAdd != "")
+                {
+                    row.push_back(newValueToAdd);
+                }
+                newValueToAdd = "";
+            }
+            else // part of the number
+            {
+                newValueToAdd += inputChar;
+            }
+
+        }
+        // End of row
+        data.push_back(row);
+    }
+
+    // Calculate the result.
 }
+
+//int inputAsInt = inputChar - '0';
