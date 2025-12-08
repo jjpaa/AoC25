c5fc4f5c69f5a0ea9e6eae7694bc431d69515fba
JJPaa <juhojarvenpaa98@gmail.com>
2025-12-08 09:19:01 +0200

    First of day 6 done.

--- Day6/Day6.cpp
+++ Day6/Day6.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 int main()
 {
-    ifstream MyReadFile("input2.txt");
+    ifstream MyReadFile("input.txt");
     vector<vector<string>> data = {};
+    long long counter = 0;
 
     while (true)
     {
@@ -28,15 +29,7 @@ int main()
         {
             char inputChar = input[i];
 
-            if (inputChar == '*')
-            {
-
-            }
-            else if (inputChar == '+')
-            {
-
-            }
-            else if (inputChar == ' ')
+            if (inputChar == ' ')
             {
                 if (newValueToAdd != "")
                 {
@@ -50,11 +43,101 @@ int main()
             }
 
         }
+        if (newValueToAdd != "")
+        {
+            row.push_back(newValueToAdd);
+        }
+        newValueToAdd = "";
+
         // End of row
         data.push_back(row);
     }
+    long long rowsAmount = data.size() - 1;
+    long long columnAmount = data[0].size() - 1;
+
+    // Print the thing
+    //for (long long r = 0; r < rowsAmount;r++)
+    //{
+    //    for (long long c = 0; c <= columnAmount; c++)
+    //    {
+    //        cout << data[r][c] << " ";
+    //    }
+    //    cout << "\n";
+    //}
 
     // Calculate the result.
-}
+    // Loop thorough values
+    // Loop through the rows
+    // vector<vector<string>> data = {};
 
-//int inputAsInt = inputChar - '0';
+    //for (long long row_i = 0; row_i < rowsAmount; row_i++)
+    //{
+    //    long long tempCounter = 1;
+
+    //    for (long long column_i = 0; column_i <= columnAmount; column_i++)
+    //    {
+    //        bool plus = false;
+    //        // Then take the operator type
+    //        auto operatorAsString = data[rowsAmount][column_i];
+
+    //        if (operatorAsString == "+")
+    //        {
+    //            plus = true;
+    //            tempCounter = 0;
+    //        }
+
+    //        long long valueAsInt = std::stoll(data[row_i][column_i]);
+
+    //        cout << "Order of printing: " << valueAsInt << "\n";
+
+    //        if (plus)
+    //        {
+    //            tempCounter = tempCounter + valueAsInt;
+    //        }
+    //        else
+    //        {
+    //            tempCounter = tempCounter * valueAsInt;
+    //        }
+    //    }
+    //    counter += tempCounter;
+    //}
+
+    for (long long column_i = 0; column_i <= columnAmount; column_i++)
+    {
+        long long tempCounter = 0;
+
+        for (long long row_i = 0; row_i < rowsAmount; row_i++)
+        {
+            bool plus = false;
+            // Then take the operator type
+            auto operatorAsString = data[rowsAmount][column_i];
+
+            if (operatorAsString == "+")
+            {
+                plus = true;
+            }
+
+            long long valueAsInt = std::stoll(data[row_i][column_i]);
+
+            //cout << "Order of printing: " << valueAsInt << "\n";
+
+            if (plus)
+            {
+                tempCounter = tempCounter + valueAsInt;
+            }
+            else
+            {
+                if (tempCounter == 0)
+                {
+                    tempCounter = 1;
+                }
+
+                tempCounter = tempCounter * valueAsInt;
+            }
+        }
+        //cout << tempCounter << "\n";
+        counter += tempCounter;
+    }
+
+    cout << counter;
+}
